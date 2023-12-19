a156c5418e0c4e17eca7d88227132185a33b18a6
Anish Kadempally <anishkadempally123@gmail.com>
2023-12-19 09:49:30 +0530

    Demo for team

---
+++ src/main/java/in/kmit/E_Ticketing/App.java
@@ -0,0 +1,13 @@
+package in.kmit.E_Ticketing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hiii" );
+    }
+}
